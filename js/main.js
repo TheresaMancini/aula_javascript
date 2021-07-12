90bcf48abc0786ae6b2dec99184f2758f223bee0
TheresaMancini <theresa.mancini@hotmail.com>
2021-07-12 18:26:00 -0300

    Arquivos depois da aula 1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let nome = "Theresa Mancini"
+let idade = 24; 
+let idade2 = 10;
+let frase = "Japão é um pais muito tecnológico."
+//alert(`${nome} tem ${idade} anos`); // também poderia ser ("bem vinda" + nome)
+//alert(idade+idade2)
+
+console.log(frase.replace("Japão","Coreia")); 
+alert(frase.replace("Japão","Coreia"));
