3f067673979caeb084888703148316cc12e78bb3
TheresaMancini <theresa.mancini@hotmail.com>
2021-07-13 11:39:06 -0300

    Depois da aula 5

--- js/main.js
+++ js/main.js
@@ -44,3 +44,71 @@ if (idade >= 18){
 let d = new Date();
 alert(d);
 */
+
+// aula 4 funções 
+
+/* 
+function soma(n1,n2){
+    return n1+n2; 
+}
+
+alert(soma(5,10))
+
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+alert(setReplace("Olá, meu nome é Maria", "Maria", "Júlia")); 
+
+function validaIdade(idade){
+    let validar;
+    if (idade>=18){
+        validar = true
+    }
+    else{
+        validar = false
+    }
+    return validar
+}
+
+let idade = prompt("Qual é a sua idade? ")
+
+console.log(validaIdade(idade))
+
+*/
+
+function clicou(){
+   // alert("Obrigada por clicar! ")
+   // agora vou editar o html através do JS
+   document.getElementById("agradecimento").innerHTML = "<b>Obrigada por clicar</b>";
+  // console.log(document.getElementById("agradecimento"))
+
+}
+
+function mudarPagina(){
+    window.open("https://www.google.com.br"); // nova aaba
+}
+function redirecionar(){
+    window.location.href = "https://www.google.com.br"; // abre na pag atual 
+}
+
+function trocar(elemento){
+   // document.getElementById("passandoMouse").innerHTML = "obridado por passar o mouse"; // dessa forma só funciona no elemento com essa ID
+   // essede baixo funciona pra qualquer elemento, pq passamos os elemento como parametro para a função
+   elemento.innerHTML = "obrigado por passar o mouse";
+
+}
+
+function voltar(elemento){
+   // document.getElementById("passandoMouse").innerHTML = "passe o mouse aqui! ";
+   elemento.innerHTML = "passe o mouse aqui!"
+}
+
+function load(){ // onload no html usado no body. Quando carregado chama a função que eu definir
+    alert("Página Carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
