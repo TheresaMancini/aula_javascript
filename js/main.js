745f99fbf8f44e40b0a47fac074296afdab02452
TheresaMancini <theresa.mancini@hotmail.com>
2021-07-12 21:07:17 -0300

    Códigos depois da aula 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 let nome = "Theresa Mancini"
 let idade = 24; 
 let idade2 = 10;
@@ -7,3 +8,39 @@ let frase = "Japão é um pais muito tecnológico."
 
 console.log(frase.replace("Japão","Coreia")); 
 alert(frase.replace("Japão","Coreia"));
+*/
+
+// aula 2
+/*
+//lista
+
+let lista = ["maçã", "pera", "laranja"]; 
+lista.push("uva")
+console.log(lista);
+lista.pop();
+alert(lista[2]);
+console.log(lista);
+console.log(lista.toString())
+console.log(lista.join("-")) // transforma em string mas unindo por aquele determinado elemento 
+
+// dicionário 
+let frutas = [{nome: "maçã", cor: "vermelho"}, {nome: "uva", cor: "roxo"},{nome: "pera", cor: "verde"}]
+
+console.log(frutas[1].nome);
+*/
+
+/*
+// aula 3
+
+// condicionais, laços de repetição e Date
+
+let idade = prompt("Qual sua idade? ");
+if (idade >= 18){
+    alert("Maior de Idade")
+}else{
+    alert("Menor de Idade")
+};
+
+let d = new Date();
+alert(d);
+*/
